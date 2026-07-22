11afcca7a58e0a9707654d4d8ae5fc46d1b138d1
riserishitha <rishitha1826@gmail.com>
2026-07-22 13:02:49 +0530

    completed implementing login functionality in frontend

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,47 @@
+import api from "../api/axios";
+import type {
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+} from "../types/auth";
+
+export const register = async (
+  data: RegisterRequest
+) => {
+  const response = await api.post(
+    "/auth/register",
+    data
+  );
+
+  return response.data;
+};
+
+export const login = async (
+  data: LoginRequest
+): Promise<LoginResponse> => {
+
+  const formData = new URLSearchParams();
+
+  formData.append(
+    "username",
+    data.email,
+  );
+
+  formData.append(
+    "password",
+    data.password,
+  );
+
+  const response = await api.post(
+    "/auth/login",
+    formData,
+    {
+      headers: {
+        "Content-Type":
+          "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  return response.data;
+};
